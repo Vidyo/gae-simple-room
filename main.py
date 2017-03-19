753b74de9dc0f8f2f609a882d5db4c03aaebff04
Brian Mwakima <mwadime@fortunekidew.co.ke>
2017-03-19 18:11:12 +0300

    Setup for heroku

--- main.py
+++ main.py
@@ -20,20 +20,21 @@ import urllib2
 import base64
 import binascii
 from datetime import datetime
-import calendar, time
+import calendar
+import time
 import hashlib
 import hmac
 import sys
 import random
 
-from google.appengine.ext import ndb
+# from google.appengine.ext import ndb
 
 import jinja2
 import webapp2
 
 # Developer specific parameters
-VIDYO_IO_DEVELOPER_KEY    = "[YOUR DEVELOER KEY]"
-VIDYO_IO_APPLICATION_ID   = "[YOUR APPLICATION ID]"
+VIDYO_IO_DEVELOPER_KEY = "[YOUR DEVELOER KEY]"
+VIDYO_IO_APPLICATION_ID = "[YOUR APPLICATION ID]"
 TOKEN_VALID_DURATION_SECS = 600
 
 EPOCH_SECONDS = 62167219200
@@ -45,58 +46,69 @@ JINJA_ENVIRONMENT = jinja2.Environment(
 
 
 def getVidyoIOToken(userName):
-	type    = 'provision'
-	key     = VIDYO_IO_DEVELOPER_KEY
-	jid     = userName + "@" + VIDYO_IO_APPLICATION_ID
-	expires = TOKEN_VALID_DURATION_SECS + EPOCH_SECONDS + int(time.mktime(datetime.now().timetuple()))
-	vCard   = ""
-	
-	def to_bytes(o):
-		return str(o).encode("utf-8")
-		
-	sep = b"\0" # Separator is a NULL character
-	body = to_bytes(type) + sep + to_bytes(jid) + sep + to_bytes(expires) + sep + to_bytes(vCard)
-	mac = hmac.new(bytearray(key, 'utf8'), msg=body, digestmod=hashlib.sha384).digest()
-	## Combine the body with the hex version of the mac
-	serialized = body + sep + binascii.hexlify(mac)
-	b64 = base64.b64encode(serialized)
-	token = b64.encode("utf8")
-	encoded_token = urllib2.quote(token)
-	return encoded_token;
+    type = 'provision'
+    key = VIDYO_IO_DEVELOPER_KEY
+    jid = userName + "@" + VIDYO_IO_APPLICATION_ID
+    expires = TOKEN_VALID_DURATION_SECS + EPOCH_SECONDS + \
+        int(time.mktime(datetime.now().timetuple()))
+    vCard = ""
+
+    def to_bytes(o):
+        return str(o).encode("utf-8")
+
+    sep = b"\0"  # Separator is a NULL character
+    body = to_bytes(type) + sep + to_bytes(jid) + sep + \
+        to_bytes(expires) + sep + to_bytes(vCard)
+    mac = hmac.new(bytearray(key, 'utf8'), msg=body,
+                   digestmod=hashlib.sha384).digest()
+    # Combine the body with the hex version of the mac
+    serialized = body + sep + binascii.hexlify(mac)
+    b64 = base64.b64encode(serialized)
+    token = b64.encode("utf8")
+    encoded_token = urllib2.quote(token)
+    return encoded_token
 
 # [START main_page]
+
+
 class MainPage(webapp2.RequestHandler):
 
-	def get(self):
-		template_values = {}
-		template = JINJA_ENVIRONMENT.get_template('index.html')
-		self.response.write(template.render(template_values))
+    def get(self):
+        template_values = {}
+        template = JINJA_ENVIRONMENT.get_template('index.html')
+        self.response.write(template.render(template_values))
 # [END main_page]
 
 # [START Room]
+
+
 class Room(webapp2.RequestHandler):
 
-	def get(self, roomId):
-		# RoomID must not have spaces or special characters. Base64 encode will ensure that.
-		roomIdBase64AndEncoded = urllib2.quote(base64.b64encode(roomId))
-		# Pick the VidyoIO client version or select latest
-		version = self.request.get('version', 'latest')
-		# Create a random username for each user
-		username = ''.join(random.choice('0123456789ABCDEF') for i in range(16))
-		
-		encoded_token = getVidyoIOToken(username)
-		url_vidyoio = "https://static.vidyo.io/" + version + "/connector/VidyoConnector.html?host=vidyostaging.io&resourceId=" + roomIdBase64AndEncoded + "&token=" + encoded_token + "&hideConfig=1&" + self.request.query_string
-		template_values = {
-			'url_vidyoio': url_vidyoio,
-		}
-		template = JINJA_ENVIRONMENT.get_template('room.html')
-		self.response.write(template.render(template_values))
+    def get(self, roomId):
+        # RoomID must not have spaces or special characters. Base64 encode will
+        # ensure that.
+        roomIdBase64AndEncoded = urllib2.quote(base64.b64encode(roomId))
+        # Pick the VidyoIO client version or select latest
+        version = self.request.get('version', 'latest')
+        # Create a random username for each user
+        username = ''.join(random.choice('0123456789ABCDEF')
+                           for i in range(16))
+
+        encoded_token = getVidyoIOToken(username)
+        url_vidyoio = "https://static.vidyo.io/" + version + "/connector/VidyoConnector.html?host=vidyostaging.io&resourceId=" + \
+            roomIdBase64AndEncoded + "&token=" + encoded_token + \
+            "&hideConfig=1&" + self.request.query_string
+        template_values = {
+            'url_vidyoio': url_vidyoio,
+        }
+        template = JINJA_ENVIRONMENT.get_template('room.html')
+        self.response.write(template.render(template_values))
 
 # [END Room]
 
 # [START app]
 app = webapp2.WSGIApplication([
-	('/', MainPage),
-	('/([^/]+)?', Room),
+    ('/', MainPage),
+    ('/([^/]+)?', Room),
 ], debug=True)
 # [END app]
